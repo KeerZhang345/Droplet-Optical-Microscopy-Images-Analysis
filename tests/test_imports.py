482f0c69649cecc32b1b6042a8e914791ff1212b
KeerZhang345 <kerrzhang34@gmail.com>
2026-01-08 22:48:53 +0100

    Create test_imports.py

---
+++ tests/test_imports.py
@@ -0,0 +1,12 @@
+def test_basic_imports():
+    import droplet_corr_lab
+
+    # top-level API
+    from droplet_corr_lab import Data_scaling
+
+    # intended public submodules
+    from droplet_corr_lab import features
+    from droplet_corr_lab import segmentation
+    from droplet_corr_lab import preprocessing
+    from droplet_corr_lab import analysis
+    from droplet_corr_lab import compute
